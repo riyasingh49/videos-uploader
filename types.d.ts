c826cdcd938651a51923bc2b1cfb4af07d8cf6b4
riyasingh49 <riyasingh454247@gmail.com>
2025-11-17 16:52:18 +0530

    authentication going on

---
+++ types.d.ts
@@ -0,0 +1,10 @@
+import connection, { Connection } from "mongoose";
+
+declare global {
+    var mongoose: {
+        conn: Connection | null;
+        promise: Promise<Connection> | null;
+    }
+}
+
+export {};
